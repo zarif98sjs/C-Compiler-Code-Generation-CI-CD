9cb082afd5cc9010c48be78724873d4663fc9467
Zarif <zarif98sjs@gmail.com>
2021-07-19 16:21:43 +0600

    optimization test added

---
+++ optimizer/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from . models import Code
+
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.code = Code("","")
+        line_v = ["MOV WORD PTR [bp-20],AX","MOV AX,[bp-20]"]
+        self.code.optimizeCode(line_v)
+
+        with open('opt_new.asm', 'r') as file:
+            data = file.read().replace('\n', '\n')
+        self.code.opt_code = data
+
+    def test_post_model(self):
+        d = self.code
+        self.assertEqual(self.code.opt_code, "MOV WORD PTR [bp-20],AX\n")
