40a7d4594a8f050046893a9c954a6c295c8205fa
Zarif <zarif98sjs@gmail.com>
2021-07-21 02:31:22 +0600

    test fixed

--- optimizer/tests.py
+++ optimizer/tests.py
@@ -5,13 +5,13 @@ from . models import Code
 class ModelTesting(TestCase):
 
     def setUp(self):
-        self.code = Code("", "")
+        self.code = Code("", "", "")
         line_v = ["MOV WORD PTR [bp-20],AX", "MOV AX,[bp-20]"]
         self.code.optimizeCode(line_v)
 
         with open('./tmp/opt_new.asm', 'r') as file:
             data = file.read().replace('\n', '\n')
-        self.code.opt_code = data
+        self.code.opt_code_asm = data
 
     def test_post_model(self):
-        self.assertEqual(self.code.opt_code, "MOV WORD PTR [bp-20],AX\n")
+        self.assertEqual(self.code.opt_code_asm, "MOV WORD PTR [bp-20],AX\n")
